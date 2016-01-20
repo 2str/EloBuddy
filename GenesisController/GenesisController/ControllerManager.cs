f2c8c2fbe5dcb72d5eddd94eae4f8cc5f02e03a7
Alweul <alweul@gmail.com>
2016-01-19 18:58:35 -0800

    Collision update!

--- GenesisController/GenesisController/ControllerManager.cs
+++ GenesisController/GenesisController/ControllerManager.cs
@@ -31,14 +31,22 @@ namespace GenesisController
             float x = state.Gamepad.LeftThumbX;
             if (x < 500 && x > -500) x = 0;
             float y = state.Gamepad.LeftThumbY;
-            if (y < 500 && y > -500) y = 0;
+            if (y < 500 && y > -500) y = 0; //Deadzone. Dont move if the movement is super minute
             const int rangeMax = 32767;
-            
-            Vector2 move = new Vector2(Player.Instance.Position.X + 300 * (x / rangeMax),
-                Player.Instance.Position.Y + 150 * (y / rangeMax));
-            Orbwalker.DisableMovement = false;
-            Orbwalker.MoveTo(move.To3DWorld());
-            Orbwalker.DisableMovement = true;
+            if (x != 0 && y != 0)
+            {
+                Vector2 move = new Vector2(Player.Instance.Position.X + 300*(x/rangeMax),
+                    Player.Instance.Position.Y + 150*(y/rangeMax));
+                if (
+                    !(NavMesh.GetCollisionFlags(move) == CollisionFlags.Building ||
+                      NavMesh.GetCollisionFlags(move) == CollisionFlags.Wall))
+                {
+                    Orbwalker.DisableMovement = false;
+                    Orbwalker.MoveTo(move.To3DWorld());
+                    Orbwalker.DisableMovement = true;
+                }
+            }
+
             if ((state.Gamepad.Buttons & GamepadButtonFlags.A) != 0)
             {
                 Orbwalker.ActiveModesFlags = Orbwalker.ActiveModes.Combo;
